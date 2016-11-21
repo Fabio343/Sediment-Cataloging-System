603b193d9ed3ad9ea64a5f7016a21b93dfe20efa
Fabio343 <fabio.carvalho.souza@usp.br>
2016-11-21 21:30:06 -0200

    Update urls.py

--- amostras/sedimentos/urls.py
+++ amostras/sedimentos/urls.py
@@ -6,11 +6,10 @@ app_name='sedimentos'
 
 urlpatterns=[
     #se refere a /resident/
-    url(r'^$',views.index,name='index'),
+    url(r'^$',views.IndexView.as_view(),name='index'),
     # se refere a uma em especifico/
-    url(r'^(?P<amostra_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
 
-    url(r'^(?P<amostra_id>[0-9]+)/Destaque$', views.Destaque, name='Destaque'),
 
 
 ]
