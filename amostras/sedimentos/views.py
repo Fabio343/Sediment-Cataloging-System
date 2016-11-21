9d422e3ec484e0a33d9c2d8b195f7e4cb8f8b132
Fabio343 <fabio.carvalho.souza@usp.br>
2016-11-21 21:33:44 -0200

    Update views.py

--- amostras/sedimentos/views.py
+++ amostras/sedimentos/views.py
@@ -1,30 +1,14 @@
-from django.shortcuts import render, get_object_or_404
-from .models import amostra, cidade
+from django.views import generic
+from .models import amostra
 
-
-
-def index (request):
-    all_amostras=amostra.objects.all()
-    return render( request, 'sedimentos/index.html', {'all_amostras': all_amostras})
-
-
-
-def detail(request,amostra_id):
-    amostra_id = get_object_or_404(amostra, pk=amostra_id)
-    return render(request,'sedimentos/detail.html',{'amostra_id':amostra_id})
-
-
-def Destaque (request, amostra_id):
-    amostra_id = get_object_or_404(amostra, pk=amostra_id)
-    try:
-        selected_cidade =amostra_id.cidade_set.get(pk=request.POST['cidade'])
-    except(KeyError, cidade.DoesNotExist):
-        return render(request,'sedimentos/detail.html',{'amostra_id':amostra_id, 'error_message': "Sem essa informação",})
-
-    else:
-        selected_cidade.is_Destaque=True
-        selected_cidade.save()
-        return render(request, 'sedimentos/detail.html', {'amostra_id': amostra_id})
-
-
-
+class IndexView(generic.ListView):
+    template_name ='sedimentos/index.html'
+    context_object_name= 'all_amostras'
+    
+    def get_queryset(self):
+        retun amostra.objects.all()
+        
+        
+class DetailView(generic.DetailView):
+    model = amostra
+    template_name='sedimentos/detail.html'
